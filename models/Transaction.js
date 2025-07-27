608e0661f194d550ad5bad449c2765dc9082fa2e
jc123x <129776216+jc123x@users.noreply.github.com>
2025-07-26 21:06:20 -0400

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  name: { type: String, required: true },
   type: { type: String, enum: ['income', 'expense'], required: true },
   amount: { type: Number, required: true },
   category: { type: String },
