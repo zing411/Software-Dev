49847d0dcaa8555e62a0002db0d80ff179adb461
Ethan <n01552868@humbermail.ca>
2025-08-10 16:21:38 -0400

    Add default layout

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
 const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
+
 const path = require('path');
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
@@ -13,7 +15,7 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+app.set('layout', 'layouts/layout');
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/finance', {
