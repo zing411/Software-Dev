cb49ee823a75d46141010a9d4640a7502b024bf8
Ethan <n01552868@humbermail.ca>
2025-08-10 12:48:56 -0400

    Add backend search filter

--- routes/transactions.js
+++ routes/transactions.js
@@ -7,6 +7,32 @@ router.get('/', async (req, res) => {
   res.render('transactions/index', { transactions: all });
 });
 
+router.get('/search', async (req, res) => {
+
+  const {type, amount, category, minAmount, maxAmount} = req.body;
+
+  const query ={
+    user: req.session.userId,
+  }
+  if (type) query.type = type;
+  if (amount) query.amount = amount;
+  if (category) query.category = category;
+  if(minAmount || maxAmount){
+    query.amount = {};
+
+    if (minAmount) query.amount = {$gte: minAmount}
+    if (maxAmount) query.amount = {$lte: maxAmount}
+  }
+
+  const filteredSearch = await Transaction.find(query);
+  res.render('transactions/index', { transactions: filteredSearch });
+});
+
+router.get('/', async (req, res) => {
+  const all = await Transaction.find({ user: req.session.userId });
+  res.render('transactions/index', { transactions: all });
+});
+
 router.get('/new', (req, res) => res.render('transactions/new'));
 router.post('/', async (req, res) => {
   await new Transaction({ ...req.body, user: req.session.userId }).save();
